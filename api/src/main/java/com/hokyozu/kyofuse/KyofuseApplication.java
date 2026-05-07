f3c44159dd744eb6c2c04d7466400593e61d812b
Hosz <murilobento197@gmail.com>
2026-05-07 15:03:34 -0300

    chore: initialize Kyofuse

---
+++ api/src/main/java/com/hokyozu/kyofuse/KyofuseApplication.java
@@ -0,0 +1,13 @@
+package com.hokyozu.kyofuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyofuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyofuseApplication.class, args);
+	}
+
+}
